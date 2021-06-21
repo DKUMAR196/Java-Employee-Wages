fd9418f4091a050d75765f0de78575ff203a71a6
DKUMAR196 <deepakmecet@gmail.com>
2021-06-21 13:39:56 +0530

    DailyEmpWage

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,22 +1,24 @@
-#! bin/bash
+#!/bin/bash -X
 
-
-public class EmpWageBuilderUC1 {
+public class EmpWageDailyUC2 {
 
 	public static void main(String[] args) {
-		//Constants
-		int IS_FULL_TIME = 1;
-		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME) {
-			
-		 System.out.println("Employee is present");
-		    }
-		
-		else {
-			System.out.println("Employee is Abscent");
-			}
-			
+				//Constants
+				int IS_PRESENT = 1;
+				int empHrs = 8;
+				int empRatePerHr = 20;
+				int salary = 0;
+						
+				//Computation
+				double empCheck = Math.floor(Math.random() * 10) % 2;
+				if (empCheck == IS_PRESENT) {  
+					
+						salary = (empHrs * empRatePerHr);
+						System.out.println(salary);
+				}
+						else {
+							salary = 0;	
+						}
+				}
 	}
 
-}
