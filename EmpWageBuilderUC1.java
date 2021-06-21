35b006a7d9f3e9ff03ffd7f61589337222ea0c66
DKUMAR196 <deepakmecet@gmail.com>
2021-06-21 15:40:21 +0530

    Using CaseStatement

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,28 +1,41 @@
-#!/bin/bash -X
-public class EmpWagePartTimeUC3 {
+#!/bin/bash -x
+public class SwitchCaseStatement {
 
 	public static void main(String[] args) {
 				//Constants
 				int isparttime = 1;
 				int fulltime = 2;
-				int empHrs = 8;
-				int empHrsparttime = 4;
-				int empRatePerHr = 20;
+				int empRatePerHrs =20;
 				int salary = 0;
-						
+				int empHrs =4;
 				//Computation
-				double empCheck = Math.floor(Math.random() * 10) % 2;
-				if (empCheck == fulltime) { }
+				switch(salary) {
+				case'A':
+				double empCheck = Math.floor(Math.random() * 10) % 3;
 				
-				double empCheck1 = Math.floor(Math.random() * 10) % 2;	
-				 if (empCheck1 == isparttime) { 
+				       if (empCheck == fulltime)
+					              
+					break;
+				case 'B' :
+			    double empCheck1 = Math.floor(Math.random() * 10) % 3;
+				
+					   if (empCheck1 == isparttime)
+						         
+					break;
 					
-				}
-				else {
-			    salary = (empHrsparttime * empRatePerHr);
-				  System.out.println(salary);
-				}
-				}
-	
+				case 'C':		
+				double empCheck2 = Math.floor(Math.random() * 10) % 3;
+				
+						   if (empCheck2 == isparttime)
+							         
+					break;
+					
+				default:
+					
+					salary = (empHrs * empRatePerHrs);
+					System.out.println(salary);
+						
+					}		
 	}
+}
 
