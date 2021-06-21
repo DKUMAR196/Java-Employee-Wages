5ca5f66afc5835e1c3975b3cb365f5b3d061630e
DKUMAR196 <deepakmecet@gmail.com>
2021-06-21 14:25:31 +0530

    Part Time Emp Wage

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,24 +1,28 @@
 #!/bin/bash -X
-
-public class EmpWageDailyUC2 {
+public class EmpWagePartTimeUC3 {
 
 	public static void main(String[] args) {
 				//Constants
-				int IS_PRESENT = 1;
+				int isparttime = 1;
+				int fulltime = 2;
 				int empHrs = 8;
+				int empHrsparttime = 4;
 				int empRatePerHr = 20;
 				int salary = 0;
 						
 				//Computation
 				double empCheck = Math.floor(Math.random() * 10) % 2;
-				if (empCheck == IS_PRESENT) {  
+				if (empCheck == fulltime) { }
+				
+				double empCheck1 = Math.floor(Math.random() * 10) % 2;	
+				 if (empCheck1 == isparttime) { 
 					
-						salary = (empHrs * empRatePerHr);
-						System.out.println(salary);
 				}
-						else {
-							salary = 0;	
-						}
+				else {
+			    salary = (empHrsparttime * empRatePerHr);
+				  System.out.println(salary);
 				}
+				}
+	
 	}
 
